b9795dce96fb84d54df9d27634adb0892611a220
Samuel McDonnell <104273103+samjmc@users.noreply.github.com>
2026-09-14 13:38:19 +0100

    fix: remove undefined storage.getTestUser call

--- server/routes/testRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { storage } from "../storage";
-
-export async function testRoute() {
-  // This will trigger Check 2 failure: undefined method
-  const user = await storage.getTestUser("test-id");
-  return user;
-}
